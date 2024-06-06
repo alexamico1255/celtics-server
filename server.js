fd37cd6937576cb22d407231ef4a8e1c62e7de94
alexamico1255 <alexamico1255@gmail.com>
2024-06-06 12:22:20 -0400

    add player

--- server.js
+++ server.js
@@ -32,7 +32,30 @@ app.post("/api/celtics", upload.single("img"), (req, res) => {
     }
     console.log("Valid")
 
+    const player = {
+      _id: players.length +1,
+      name: req.body.name,
+      ppg: req.body.ppg,
+      rebounds: req.body.rebounds,
+      assists: req.body.assists,
+      height: req.body.height,
+      weight: req.body.weight,
+      age: req.body.age,
+      college: req.body.college,
+      draft_pick: req.body.draft_pick,
+      drafted_by: req.body.drafted_by,
+    
+    };
+
+    if (req.file) {
+      player.img_name = "images/" + req.file.filename;
+    }
+
+    players.push(player);
+    res.status(200).send(player);
+
 });
+
 
 let record = {
     "easternTeams": [
@@ -70,6 +93,136 @@ let record = {
     ]
   }
   
+ let players = [
+    {
+        "_id": 1,
+        "img_name": "images/jrue_holiday.jpg",
+        "name": "Jrue Holiday",
+        "ppg": 12.5,
+        "rebounds": 5.4,
+        "assists": 4.8,
+        "height": "6'4",
+        "weight": "205lb",
+        "age": 33,
+        "college": "UCLA",
+        "draft_pick": "R1 Pick 17",
+        "drafted_by": "Philadelphia 76ers",
+        "stats": ["12.5", "5.4", "4.8"]
+    },
+    {
+        "_id": 2,
+        "img_name": "images/derrick_white.jpg",
+        "name": "Derrick White",
+        "ppg": 15.2,
+        "rebounds": 4.2,
+        "assists": 5.2,
+        "height": "6'4",
+        "weight": "190lb",
+        "age": 29,
+        "college": "Colorado",
+        "draft_pick": "R1 Pick 29",
+        "drafted_by": "San Antonio Spurs",
+        "stats": ["15.2", "4.2", "5.2"]
+    },
+    {
+        "_id": 3,
+        "img_name": "images/jaylen_brown.jpg",
+        "name": "Jaylen Brown",
+        "ppg": 23,
+        "rebounds": 5.5,
+        "assists": 3.6,
+        "height": "6'6",
+        "weight": "223lb",
+        "age": 27,
+        "college": "California",
+        "draft_pick": "R1 Pick 3",
+        "drafted_by": "Boston Celtics",
+        "stats": ["23", "5.5", "3.6"]
+    },
+    {
+        "_id": 4,
+        "img_name": "images/jayson_tatum.jpg",
+        "name": "Jayson Tatum",
+        "ppg": 26.9,
+        "rebounds": 8.1,
+        "assists": 4.9,
+        "height": "6'8",
+        "weight": "210lb",
+        "age": 26,
+        "college": "Duke",
+        "draft_pick": "R1 Pick 3",
+        "drafted_by": "Boston Celtics",
+        "stats": ["26.9", "8.1", "4.9"]
+    },
+    {
+        "_id": 5,
+        "img_name": "images/kristaps_porzingis.jpg",
+        "name": "Kristaps Porzingis",
+        "ppg": 20.1,
+        "rebounds": 7.2,
+        "assists": 2,
+        "height": "7'2",
+        "weight": "240lb",
+        "age": 28,
+        "college": "N/A (Latvia)",
+        "draft_pick": "R1 Pick 4",
+        "drafted_by": "New York Knicks",
+        "stats": ["20.1", "7.2", "2"]
+    },
+    {
+        "_id": 6,
+        "img_name": "images/al_horford.jpg",
+        "name": "Al Horford",
+        "ppg": 18.6,
+        "rebounds": 6.4,
+        "assists": 2.6,
+        "height": "6'9",
+        "weight": "240lb",
+        "age": 37,
+        "college": "Florida",
+        "draft_pick": "R1 Pick 3",
+        "drafted_by": "Atlanta Hawks",
+        "stats": ["18.6", "6.4", "2.6"]
+    },
+    {
+        "_id": 7,
+        "img_name": "/src/images/derrick_white.jpg",
+        "name": "Peyton Pritchard",
+        "ppg": 9.6,
+        "rebounds": 3.2,
+        "assists": 3.4,
+        "height": "6'1",
+        "weight": "195lb",
+        "age": 26,
+        "college": "Oregon",
+        "draft_pick": "R1 Pick 26",
+        "drafted_by": "Boston Celtics",
+        "stats": ["9.6", "3.2", "3.4"]
+    },
+    {
+        "_id": 8,
+        "img_name": "images/sam_hauser.jpg",
+        "name": "Sam Hauser",
+        "ppg": 9.0,
+        "rebounds": 3.5,
+        "assists": 1.0,
+        "height": "6'7",
+        "weight": "217lb",
+        "age": 26,
+        "college": "Virginia",
+        "draft_pick": "Undrafted",
+        "drafted_by": "NA",
+        "stats": ["9.0", "3.5", "1.0"]
+
+        
+    }
+]
+
+
+app.get("/api/players", (req, res) => {
+  res.send(players);
+});
+
 app.get("/api/record", (req, res) => {
     res.send(record);
 });
@@ -88,6 +241,42 @@ const validatePlayer = (player) => {
     return schema.validate(player);
 };
 
+app.put("/api/players/:id", upload.single("image"), (req, res) => {
+  let player = players.find((e) => e._id === parseInt(req.params.id));
+console.log("Before validate");
+console.log("Player is " + player.name);
+  if (!player) {
+    res.status(400).send("Player with given id was not found");
+    return;
+  }
+console.log("after validate");
+
+      player.name = req.body.name;
+      player.ppg = req.body.ppg;
+      player.rebounds = req.body.rebounds;
+      player.assists = req.body.assists;
+      player.height = req.body.height;
+      player.weight = req.body.weight;
+      player.age = req.body.age;
+      player.college = req.body.college;
+      player.draft_pick = req.body.draft_pick;
+      player.drafted_by = req.body.drafted_by;
+
+      if (req.file) {
+        player.img_name = "images/" + req.file.filename;
+      }
+
+  const result = validatePlayer(req.body);
+
+  if (result.error) {
+    res.status(400).send(result.error.details[0].message);
+    return;
+  }
+
+  
+  res.send("Player successfully updated");
+});
+
 app.listen(3001, () => {
-    console.log("I'm listening");
+  console.log("I'm listening");
 });
